3cdc8d8f5499c3166b36621d4bd7a6d464ebe473
chenishi <chen.ishi@gmail.com>
2021-06-21 18:06:19 +0800

    Finish sanity test for clustering

--- cluster/tree-cluster.py
+++ cluster/tree-cluster.py
@@ -5,38 +5,38 @@ from sklearn.cluster import KMeans
 import json, logging
 
 def one_hot_encoder(inputs: list, size:int):
-	ret_list = [] * size
-	for set_bit in inputs:
-		ret_list[set_bit] = 1
-	return ret_list
+    ret_list = [0] * size
+    for set_bit in inputs:
+        ret_list[int(set_bit)] = 1
+    return ret_list
 
 
 if __name__ == "__main__":
-	parser = ArgumentParser()
-	parser.add_argument("-p", "--pod", help="Number of pod in network topology", dest="pod", type=int, default=30)
-	parser.add_argument("-q", "--query", help="Number of overall queries", dest="query", type=int, default=1000)
-	parser.add_argument("-c", "--cluster", help="Number of query cluster", dest="cluster", type=int, default=20)
+    parser = ArgumentParser()
+    parser.add_argument("-p", "--pod", help="Number of pod in network topology", dest="pod", type=int, default=30)
+    parser.add_argument("-q", "--query", help="Number of overall queries", dest="query", type=int, default=1000)
+    parser.add_argument("-c", "--cluster", help="Number of query cluster", dest="cluster", type=int, default=20)
 
-	args = parser.parse_args()
+    args = parser.parse_args()
 
-	logging.basicConfig(level=logging.WARNING)
-
+    logging.basicConfig(level=logging.WARNING)
+        
     with open("/home/lthpc/git/catchment-basin-seeker/data/queries-v2.json") as input:
         unparsed_queries = input.read()
 
-	# TODO: ADD support for multiple imbalanced sources in gen_query module
-	queries = json.loads(unparsed_queries)
+    # TODO: ADD support for multiple imbalanced sources in gen_query module
+    queries = json.loads(unparsed_queries)
 
-	flat_queries = []
-	for query in queries:
-		flat_queries.append(one_hot_encoder(inputs: query["src"], size: args.pod))
+    flat_queries = []
+    for query in queries:
+        flat_queries.append(one_hot_encoder(inputs=query["src"], size=args.pod))
 
-	logging.info("Done preprocess.")
+    logging.info("Done preprocess.")
 
-	kmeans = KMeans(n_clusters=args.cluster)
-	kmeans.fit(X)
-	print(kmeans.labels_)
-	# y_kmeans = kmeans.predict(X)
+    kmeans = KMeans(n_clusters=args.cluster)
+    kmeans.fit(flat_queries)
+    print(kmeans.labels_)
+    # y_kmeans = kmeans.predict(X)
 
 
 
