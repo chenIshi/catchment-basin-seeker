ebab17c2f4c32183640a1e97676d30fb8b0a673d
chenishi <chen.ishi@gmail.com>
2021-07-22 17:46:51 +0800

    Add query generator with centrialed pods

--- data/gen_query-v3.py
+++ data/gen_query-v3.py
@@ -42,8 +42,10 @@ if __name__ == "__main__":
     parser.add_argument("-p", "--pod", help="Number of pod in network topology", dest="pod", type=int, default=30)
     parser.add_argument("-q", "--query", help="Number of overall queries", dest="query", type=int, default=1000)
     parser.add_argument("-s", "--size", help="Number of pod with src pod and dst pod", dest="size", type=int, default=4)
-    # mode 0 for uniform distribution, mode 1 for src-only imbalance, mode 2 for src and dst imbalance
+    # mode 0 for uniform distribution, mode 1 for src-only imbalance, mode 2 for src and dst imbalance, mode 3 for all-to-all query, mode 4 for probabal mode 3
     parser.add_argument("-m", "--mode", help="Query distribution mode", dest="mode", type=int, default=0)
+    parser.add_argument("-c", "--chance", help="Chance for mode 4 to apply all2all", dest="chance", type=int, default=50)
+     parser.add_argument("-a", "--all2all", help="Involved pod within a all2all query", dest="inv_pods", type=int, default=3)
     # rate render no use when in mode 0
     parser.add_argument("-r", "--rate", help="Query imbalance rate (int in percentage)", dest="rate", type=int, default=50)
     parser.add_argument("-o", "--output", help="Output json location", dest="output", default="/home/lthpc/git/catchment-basin-seeker/data/queries-v3.json")
@@ -57,12 +59,36 @@ if __name__ == "__main__":
 
     imbalanced_src = distinct_random_numList_generator(pod_num=args.pod, size=args.size)
     imbalanced_dst = distinct_random_numList_generator(pod_num=args.pod, size=args.size, isDuplicate=imbalanced_src)
-    for _ in range(args.query):
-        if args.mode == 2 and random.random() * 100 < args.rate:
+    current_qentry_num = 0
+    while current_qentry_num < args.query:
+        if args.mode == 3 or (args.mode == 4 and current_qentry_num < args.query - ((args.inv_pods * (args.inv_pods - 1))/2) and random.random() * 100 > args.chance):
+            if args.inv_pods <= 2:
+                logging.error("Invalid parameter: inv_pods when all2all semantic is applied")
+                exit()
+            else:
+                inv_pods = [0] * args.inv_pods
+                prev_pods = []
+                current_qentry_num += ((args.inv_pods * (args.inv_pods - 1))/2)
+                # generate arbitary number of distinct switch sets
+                for pods in inv_pods:
+                    pods = distinct_random_numList_generator(pod_num=args.pod, size=args.size, isDuplicate=prev_pods)
+                    prev_pods.extend(pods)
+                # since it is a all to all query, we have to span through every possible query combination
+                for idx, srcPods in enumerate(range(args.inv_pods)):
+                    for dstPodsIdx in range(idx+1, args.inv_pods):
+                        queries.append({
+                            "src": srcPods,
+                            "dst": inv_pods[dstPodsIdx]
+                        })
+                    
+                
+                    
+        elif args.mode == 2 and random.random() * 100 < args.rate:
             queries.append({
                 "src": imbalanced_src,
                 "dst": imbalanced_dst
             })
+            current_qentry_num += 1
         else:
             if args.mode == 1 and random.random() * 100 < args.rate:
                 src = imbalanced_src
@@ -74,6 +100,7 @@ if __name__ == "__main__":
                 "src": src,
                 "dst": dst
             })
+            current_qentry_num += 1
             
     with open(args.output, 'w') as json_file:
         json.dump(queries, json_file)
