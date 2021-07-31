60b709676ccadbce9f61bcc6cc4f46af800af7b5
chenishi <chen.ishi@gmail.com>
2021-07-31 18:19:53 +0800

    Add entry estimation on clustering

--- cluster/tree-cluster.py
+++ cluster/tree-cluster.py
@@ -60,7 +60,34 @@ if __name__ == "__main__":
         for idx in sources[i]:
             cost += distance.hamming(products[i], flat_queries[idx])
 			
-    print("Overall cost = %d" % (cost))
+    print("Overall Bandwidht cost = %d" % (cost))
+
+    switch_load_wi_cluster = [0] * 512
+    switch_load_wo_cluster = [0] * 512
+    sw_in_pod = 16
+
+    for idx in range(8):
+        switch_load_wi_cluster[idx] += (len(sources) * 8)
+        switch_load_wi_cluster[256+idx] += len(sources)
+        switch_load_wo_cluster[idx] += (args.query * 8)
+        switch_load_wo_cluster[256+idx] += len(args.query)
+    # With cluster
+    for product in products:
+        for pod in product:
+            for aggr_idx in range(8):
+                switch_load_wi_cluster[pod*8 + aggr_idx] += 1
+
+    # Without cluster
+    for source in sources:
+        for pod in source:
+            for aggr_idx in range(8):
+                switch_load_wo_cluster[pod*8 + aggr_idx] += 1
+
+
+    avg_wo_cluster = sum(switch_load_wo_cluster) / float(len(switch_load_wo_cluster))
+    avg_wi_cluster = sum(switch_load_wi_cluster) / float(len(switch_load_wi_cluster))
+    print("Avg wo cluster = %d, wi cluster = %d" % (avg_wo_cluster, avg_wi_cluster))
+
 
 
 
